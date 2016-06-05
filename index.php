a3b122f97b084bd06534e0a31108b1690cc78485
beaujibby <Zachary@computer.local>
2016-06-05 08:28:11 -0700

    Init
    Initial Framework

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>astrum</title>
+<link rel='stylesheet' type='text/css' href='css/loginsheet.css'/>
+<link rel='stylesheet' type='text/css' href='css/materialize.css'/>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/materialize.js"></script>
+
+</head>
+<body style="background:#3B3F51">
+
+<div class="row">
+<div class="col s12">
+<ul class="tabs">
+<li class="tab col s3"><a class="active" href="#home">Home</a></li>
+<li class="tab col s3"><a href="#games">Games</a></li>
+<li class="tab col s3"><a href="#catalog">Catalog</a></li>
+<li class="tab col s3"><a href="#users">Users</a></li>
+<li class="tab col s3"><a href="#forum">Forum</a></li>
+</ul>
+</div>
+    
+</div>
+    
+<div id="home" class="row">
+    <div class="col s3 offset-s1">
+        <img src="blogo.png" style="width:100%;height:100%">
+    </div>
+    <div class="card col s7" style="color:#3B3F51">
+        <h1>beaujibby</h1>
+        <div class="border" style="height:1px;background-color:#ee6e73"></div>
+        <h6 style="word-wrap:break-word">about hello world who are you i am me who are not me you are not me yeah tyler is gay its true and you know eet</h6>
+    </div>
+    <div class="card col s10 offset-s1" style="color:#3B3F51">
+        <h3>Achievements</h3>
+        <div class="border" style="height:1px;background-color:#ee6e73"></div>
+        <img src="/svg/gameboy.svg" title="From the 80s">
+        <img src="/svg/program.svg" title="Made this thing">
+        <img src="/svg/tank.svg" title="Crashed a tank">
+        <img src="/svg/smile.svg" title="Winked at someone">
+        <img src="/svg/football.svg" title="Played footbal">
+        <img src="/svg/bamboo.svg" title="Found some bamboo">
+    </div>
+    <div class="card col s10 offset-s1" style="color:#3B3F51">
+        <h3>Statistics</h3>
+        <div class="border" style="height:1px;background-color:#ee6e73"></div>
+        <div class="col s3"><h5 style="text-align:center">Date Joined</h5><h5 style="text-align:center">06 03 16</h6></div>
+        <div class="col s3"><h5 style="text-align:center">Forum Posts</h5><h5 style="text-align:center">0</h6></div>
+        <div class="col s3"><h5 style="text-align:center">Friends</h5><h5 style="text-align:center">0</h6></div>
+        <div class="col s3"><h5 style="text-align:center">User Number</h5><h5 style="text-align:center">1</h6></div>
+    </div>
+</div>
+    
+<div id="games" class="row">Test 2</div>
+<div id="catalog" class="row">Test 3</div>
+<div id="users" class="row">Test 4</div>
+<div id="forum" class="row">Test 5</div>
+
+<footer class="page-footer">
+<div class="container">
+<div class="row">
+<div class="col l6 s12">
+<h5 class="white-text">Footer Content</h5>
+<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+</div>
+<div class="col l4 offset-l2 s12">
+<h5 class="white-text">Links</h5>
+<ul>
+<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="footer-copyright">
+<div class="container">
+© 2014 Copyright Text
+<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+</div>
+</div>
+</footer>
+
+<?php
+?>
+</body>
+</html>
